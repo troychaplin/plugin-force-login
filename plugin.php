8b42af2ca9ea2dd88e2607b6a7db1e30fe73e0b6
Troy Chaplin <info@troychaplin.ca>
2017-08-04 09:26:18 -0400

    Adding functionality

---
+++ plugin.php
@@ -0,0 +1,24 @@
+<?php
+/*
+	Plugin Name: WordPress Force Login
+	Plugin URI: http://troychaplin.ca
+	Description: Forces login and redirects user to homepage
+	Version: 1.0
+	Author: Troy Chaplin
+	Author URI: http://troychaplin.ca
+	License: GPL2
+*/
+
+function plugin_wp_force_login() {
+	is_user_logged_in() || auth_redirect();
+}
+add_action( 'parse_request', 'p2fl_force_login', 1 );
+
+// Remove Adminbar for all users, except admins
+add_action( 'after_setup_theme', 'remove_admin_bar' );
+
+function remove_admin_bar() {
+	if ( ! current_user_can( 'manage_options' ) && ! is_admin() ) {
+		show_admin_bar( false );
+	}
+}
